40b28bbfd53a89a5b4df07e994fd4547e235b424
Anna <annas.wieclaw@gmail.com>
2019-06-05 21:01:16 +0100

    Do not show the form if you already know when bday is

--- app.rb
+++ app.rb
@@ -2,11 +2,22 @@ require 'sinatra'
 require 'time'
 require 'date'
 
+enable :sessions
+set :session_secret, "something"
 get '/your-bday' do
+  @name = session[:name]
+  @day = session[:day]
+  @month = session[:month]
+  erb :index
+end
+
+get '/your-bday2' do
+
   erb :index
 end
 
 post '/bday' do
+  session[:name] = params[:name]
   @name = params[:name]
   @day = params[:day].to_i
   @month = params[:month].to_i
@@ -16,5 +27,7 @@ post '/bday' do
   @month <= @today.month ? year = @today.year + 1 : year = @today.year
   finish_date = Date.new(year, @month, @day)
   @calc = finish_date - start_date
+  session[:day] = @day
+  session[:month] = @month
   erb :second_page
 end
