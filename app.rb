49a3e21ed52dd7ad6f85a41f3bdb67088cac1f57
Anna <annas.wieclaw@gmail.com>
2019-06-05 20:25:57 +0100

    Improve logic to work for current month and eralier dates

--- app.rb
+++ app.rb
@@ -11,8 +11,10 @@ post '/bday' do
   @day = params[:day].to_i
   @month = params[:month].to_i
   @today = Time.now
+  year = 0
   start_date = Date.new(@today.year, @today.month, @today.day)
-  finish_date = Date.new(@today.year, @month, @day)
+  @month <= @today.month ? year = @today.year + 1 : year = @today.year
+  finish_date = Date.new(year, @month, @day)
   @calc = finish_date - start_date
   erb :second_page
 end
